6cc9421d8c7010c9e2aaba6f120c57048ca4bf13
carolina hansen <laaflaka.8@gmail.com>
2021-06-06 20:29:55 -0300

    Aplicacion de primeras directivas y pipes

---
+++ src/app/products-list/Products.ts
@@ -0,0 +1,9 @@
+//defino la interface producto. de manera publica(export)
+export interface Product {
+    name: string;
+    price: number;
+    stock: number;
+    img: string;
+    clearance: boolean;
+
+}
--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './products';
 
 @Component({
   selector: 'app-products-list',
@@ -6,12 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-list.component.scss']
 })
 export class ProductsListComponent implements OnInit {
-  producto = {
-    "nombre": "Remera dama gris",
-    "precio": 600,
-    "stock":  10,
-    "imagen" : "assets/img/remeraDamaAzul2.jpg"
+  products: Product[] = [
+    {
+    name: "Remera dama gris",
+    price: 600,
+    stock:  10,
+    img : "assets/img/remeraDamaAzul2.jpg",
+    clearance: false,
+  },
+  {
+    name: "Remera dama azul",
+    price: 500,
+    stock:  12,
+    img : "assets/img/remeraDamaAzul2.jpg",
+    clearance: true,
+  },
+  {
+    name: "Remera hombre roja",
+    price: 700,
+    stock:  0,
+    img : "assets/img/remeraDamaAzul2.jpg",
+    clearance: false,
   }
+]
 
   constructor() { }
 
